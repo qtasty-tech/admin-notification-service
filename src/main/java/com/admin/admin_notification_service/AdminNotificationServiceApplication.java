01950387a86f5ec76e1be0c1e3bfa82fbc101e28
ThisaraJayas <sasmithajayasinghe1@gmail.com>
2025-04-26 18:22:57 +0530

    initial

---
+++ src/main/java/com/admin/admin_notification_service/AdminNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.admin.admin_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminNotificationServiceApplication.class, args);
+	}
+
+}
